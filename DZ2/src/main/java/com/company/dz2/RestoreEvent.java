c90f2548567c2ad8d90cb060e84dbef2fb16c734
Korolenko Roman <korolenko.as1204@asugubkin.ru>
2019-09-19 08:59:58 +0300

    Code refactor

--- DZ2/src/main/java/com/company/dz2/RestoreEvent.java
+++ DZ2/src/main/java/com/company/dz2/RestoreEvent.java
@@ -2,7 +2,6 @@ package com.company.dz2;
 
 public class RestoreEvent extends Event {
     public RestoreEvent(int deviceId, int componentId){
-        this.deviceId = deviceId;
-        this.componentId = componentId;
+        super(deviceId, componentId);
     }
 }
