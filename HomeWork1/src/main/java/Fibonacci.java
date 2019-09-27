3bac165553fc6f136d68bb50e66e1d2994193e56
Roman Korolenko <korolenko.as1204@asugubkin.ru>
2019-09-27 13:57:33 +0300

    Code refactoring

--- HomeWork1/src/main/java/Fibonacci.java
+++ HomeWork1/src/main/java/Fibonacci.java
@@ -4,33 +4,30 @@ import java.util.Scanner;
 public class Fibonacci {
 
     public void doFibonachi() throws InputMismatchException{
-        int amount;
+        int amount=0;
         System.out.println("Welcome to Fibonachi numbers creator!");
-        while (true){
+        while (amount!=999){
             System.out.println("\nPlease enter the amount of numbers or 999 for exit..");
             Scanner in = new Scanner(System.in);
             amount = in.nextInt();
-            if(amount<0){
-                System.out.println("Please, enter a number >=0");
-            }
-            else if(amount==999)
-            {
-                break;
-            }
-            else if(amount == 0){
-                System.out.println("Fibonachi sequence is zero");
-            }
-            else {
-                this.createFibonacciNumbers(amount);
-            }
+            this.createFibonacciNumbers(amount);
         }
     }
     //main logic
     private void createFibonacciNumbers(int amount) {
         int a, b, next;
-        a = 0;
-        b = 1;
-        if(amount==1){
+        a = 0;b = 1;
+        if(amount<0){
+            System.out.println("Please, enter a number >=0");
+        }
+        else if(amount==999)
+        {
+            return;
+        }
+        else if(amount == 0){
+            System.out.println("Fibonachi sequence is zero");
+        }
+        else if(amount==1){
             System.out.println(a);
         }
         else {
