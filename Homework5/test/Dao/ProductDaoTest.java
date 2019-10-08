a292ab2bd950eaeeb101c35797d7e7bf68f796ca
Korolenko Roman <korolenko.as1204@asugubkin.ru>
2019-10-08 22:07:04 +0300

    Add Mockito test

--- Homework5/test/Dao/ProductDaoTest.java
+++ Homework5/test/Dao/ProductDaoTest.java
@@ -3,44 +3,48 @@ package Dao;
 import Entities.Product;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ProductDaoTest {
 
+    private ProductDao productDao;
+    private Product product;
+
     @Mock
-    private EntityTransaction transaction;
+    private EntityManagerFactory entityManagerFactory;
+
+    @Mock
+    private EntityTransaction entityTransaction;
 
     @Mock
     private EntityManager entityManager;
 
-    @Mock
-    private ProductDao productDao;
-    @Mock
-    private Product product;
-
     @Before
-    public void init(){
-        //doNothing().when(transaction).commit();
-        //Mockito.when(this.entityManager.find(Product.class, 2)).thenReturn(this.product);
-        //doNothing().when(this.product).setModel("test");
-        //doNothing().when(entityManager).close();
-        //productDao = Mockito.mock(ProductDao.class);
+    public void setUp(){
+        product = new Product("testMaker", "testModel", "testType");
+        Mockito.when(entityManagerFactory.createEntityManager()).thenReturn(entityManager);
+        Mockito.when(entityManager.getTransaction()).thenReturn(entityTransaction);
+        productDao = new ProductDao(entityManager);
     }
 
     @Test
-    public void testUpdateProduct() {
-        productDao = new ProductDao(entityManager);
-        //Mockito.when(this.entityManager.find(Product.class, 2)).thenReturn(this.product);
-        //doNothing().when(productDao).updateProduct(2, "testModel");
-        verify(productDao).updateProduct(2,"testModel");
-        //Mockito.verify(productDao, times(1)).updateProduct(2,"testModel");
-    }
+    public void testInsertProduct(){
+        this.productDao.insertProduct(product);
 
+        Mockito.verify(entityManager, times(1)).getTransaction();
+        Mockito.verify(entityTransaction, times(1)).begin();
+        Mockito.verify(entityManager, times(1)).persist(product);
+        Mockito.verify(entityTransaction, times(1)).commit();
+        Mockito.verify(entityManager, times(1)).close();
+    }
 }
