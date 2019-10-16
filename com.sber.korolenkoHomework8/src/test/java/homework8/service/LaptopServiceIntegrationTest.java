2cbfbb8af2617809508010c6cdab2cb96d52789e
Korolenko Roman <korolenko.as1204@asugubkin.ru>
2019-10-16 22:54:40 +0300

    Create LaptopServiceIntegrationTest.java

---
+++ com.sber.korolenkoHomework8/src/test/java/homework8/service/LaptopServiceIntegrationTest.java
@@ -0,0 +1,24 @@
+package homework8.service;
+
+import homework8.dta.LaptopDTO;
+import homework8.models.LaptopEntity;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LaptopServiceIntegrationTest {
+    @Autowired
+    private LaptopService laptopService;
+
+    @Test
+    public void saveTest(){
+        LaptopDTO testLaptopDTO = new LaptopDTO();
+        testLaptopDTO.setPrice(233);
+        LaptopEntity laptopEntity = laptopService.create(testLaptopDTO);
+
+        LaptopDTO actual = laptopService.findById(laptopEntity.getId());
+        Assert.assertEquals("233",actual.getPrice());
+    }
+}
